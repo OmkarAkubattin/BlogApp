87efb218fc7a9189f7970289f2932c1bb04d25b7
OmkarAkubattin <akubattino11@gmail.com>
2025-03-30 09:31:18 +0530

    main

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,13 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.send(process.env.PORT || 5000);
+ });
+app.get('/api', (req, res) => {
+ res.send("working");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
 
