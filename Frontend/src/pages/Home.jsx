1f6638935c057047cd186aa2c42e2e12f7d2a098
OmkarAkubattin <akubattino11@gmail.com>
2025-03-30 09:57:43 +0530

    main

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
           <div className="space-y-6">
             {blogs.map(blog => (
               <div key={blog._id} className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300">
-                <h2 className="text-2xl font-semibold text-gray-800">{blog.title}</h2>
+                <h2 className="text-2xl font-semibold text-gray-800">{blog.title}<span className="text-right">{blog.updatedAt}</span></h2>
                 <p className="text-gray-600 mt-2">{blog.content.substring(0, 150)}...</p>
                 <p className="text-gray-600 mt-2">Author : @{blog.author.name}</p>
                 <Link to={`/blog/${blog._id}`} className="inline-block mt-3 text-blue-600 font-semibold hover:underline">
