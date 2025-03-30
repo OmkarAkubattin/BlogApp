ca0df7f78a509750b7426269dfff1fab027247c7
OmkarAkubattin <akubattino11@gmail.com>
2025-03-30 09:42:30 +0530

    main

--- Backend/server.js
+++ Backend/server.js
@@ -11,10 +11,24 @@ const blogRoutes = require("./src/routes/blogRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://blog-app-one-tau-34.vercel.app", // Frontend URL
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "https://blog-app-one-tau-34.vercel.app",
+    "https://blog-yvzoqwchk-omkarakubattins.vercel.app"
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  next();
+});
+
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
